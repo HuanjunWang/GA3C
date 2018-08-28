f3011df190bd00a208c42dcc573c7739f4c31b69
HuanjunWang <153147512@qq.com>
2018-08-28 12:45:27 +0800

    save load

--- qlearning/qlearning.py
+++ qlearning/qlearning.py
@@ -1,6 +1,7 @@
 import random
 
 import gym
+import os
 import tensorflow as tf
 import numpy as np
 
@@ -22,58 +23,56 @@ class QNetwork:
             self.sess.run(tf.global_variables_initializer())
             self._create_tensor_board()
             tf.keras.backend.set_session(self.sess)
+            vars = tf.global_variables()
+            self.saver = tf.train.Saver({var.name: var for var in vars}, max_to_keep=0)
 
+    def _checkpoint_filename(self, episode):
+        return '%s/checkpoints/%s_%08d' % (self.log_dir, self.model_name, episode)
 
-    def train(self, state, action, target):
+    def save(self, episode):
+        self.saver.save(self.sess, self._checkpoint_filename(episode))
+
+    def load(self, episode=None):
+        filename = tf.train.latest_checkpoint(os.path.dirname(self._checkpoint_filename(episode=0)))
+        if episode is not None:
+            filename = self._checkpoint_filename(episode)
+        self.saver.restore(self.sess, filename)
+
+    def train(self, state, action, target, lr):
         loss, _, summary, step = self.sess.run([self.loss, self.train_op, self.summary_op, self.global_step],
                                                feed_dict={self.ph_observation: state, self.ph_target: target,
-                                                          self.ph_action: action})
+                                                          self.ph_action: action, self.ph_learning_rate: lr})
 
         self._copy_to_target_nn()
-
         self.log_writer.add_summary(summary, step)
         return loss
 
-    def train_batch(self, state, action, reward, next_state, done):
+    def train_batch(self, state, action, reward, next_state, done, lr):
         next_q = self.sess.run(self.max_output, feed_dict={self.ph_observation: next_state})
         not_done = ~done
         target = reward + self.gamma * next_q * not_done
-        self.train(state, action, target)
+        self.train(state, action, target, lr)
 
     def predict_q_value(self, x):
         q_values = self.sess.run(self.output, feed_dict={self.ph_observation: x})
         return q_values
 
-    def predict_action(self, x):
-        actions = self.sess.run(self.action, feed_dict={self.ph_observation: x})
-        return actions
-
     def predict_boltzmann(self, x):
-        q_values = self.sess.run(self.output, feed_dict={self.ph_observation: x})
-        q_values = q_values.astype('float64')
-        exp_values = np.exp(np.clip(q_values, -500, 500))
-        probs = exp_values / np.sum(exp_values)
+        probs = self.sess.run(self.prob, feed_dict={self.ph_observation: x})
         action = np.random.choice(range(self.num_actions), p=probs[0])
         return action
 
-    def save(self, episode):
-        pass
-
-    def load(self):
-        pass
+    def predict_action(self, x):
+        q_values = self.sess.run(self.output, feed_dict={self.ph_observation: x})
+        action = np.argmax(q_values, axis=1)
+        return action[0]
 
     def _create_graph(self):
         self.ph_observation = tf.placeholder(tf.float32, shape=[None, self.observation_dim], name="Observation")
         self.ph_target = tf.placeholder(tf.float32, shape=[None], name="TDTarget")
         self.ph_action = tf.placeholder(tf.int32, shape=[None], name="Action")
+        self.ph_learning_rate = tf.placeholder(tf.float32, name="LearningRate")
         self.global_step = tf.Variable(0, trainable=False, name='step')
-
-        self.inputs = tf.keras.Input(tensor=self.ph_observation)
-        self.l1 = tf.keras.layers.Dense(units=16, activation=tf.nn.relu, name="Layer1")(self.inputs)
-        self.l2 = tf.keras.layers.Dense(units=16, activation=tf.nn.relu, name="Layer2")(self.l1)
-        self.l3 = tf.keras.layers.Dense(units=16, activation=tf.nn.relu, name="Layer3")(self.l2)
-        self.output = tf.keras.layers.Dense(units=self.num_actions, activation=None, name="Q_Out")(self.l3)
-        self.model = tf.keras.Model(self.inputs, self.output)
 
         self.inputs_t = tf.keras.Input(tensor=self.ph_observation)
         self.l1_t = tf.keras.layers.Dense(units=16, activation=tf.nn.relu)(self.inputs_t)
@@ -82,8 +81,15 @@ class QNetwork:
         self.output_t = tf.keras.layers.Dense(units=self.num_actions, activation=None)(self.l3_t)
         self.model_target = tf.keras.Model(self.inputs_t, self.output_t)
 
+        self.inputs = tf.keras.Input(tensor=self.ph_observation)
+        self.l1 = tf.keras.layers.Dense(units=16, activation=tf.nn.relu, name="Layer1")(self.inputs)
+        self.l2 = tf.keras.layers.Dense(units=16, activation=tf.nn.relu, name="Layer2")(self.l1)
+        self.l3 = tf.keras.layers.Dense(units=16, activation=tf.nn.relu, name="Layer3")(self.l2)
+        self.output = tf.keras.layers.Dense(units=self.num_actions, activation=None, name="Q_Out")(self.l3)
+        self.prob = tf.nn.softmax(self.output)
+        self.model = tf.keras.Model(self.inputs, self.output)
+
         self.max_output = tf.reduce_max(self.output_t, axis=1, name="MaxQValue")
-        self.action = tf.argmax(self.output, axis=1, name="PredictAction")
 
         with tf.name_scope("Loss"):
             self.N = tf.shape(self.ph_action)[0]
@@ -91,7 +97,7 @@ class QNetwork:
             self.yy = tf.gather_nd(self.output, self.index)
             self.loss = tf.losses.mean_squared_error(self.ph_target, self.yy)
 
-        self.opt = tf.train.AdamOptimizer(learning_rate=1e-3)
+        self.opt = tf.train.AdamOptimizer(learning_rate=self.ph_learning_rate)
         self.train_op = self.opt.minimize(self.loss, global_step=self.global_step)
 
     def _copy_to_target_nn(self):
@@ -108,14 +114,18 @@ class QNetwork:
         self.log_writer = tf.summary.FileWriter(self.log_dir, self.graph)
 
 
-def q_learning(env_name, iter, render, gamma=.98, seed=0):
+def q_learning(env_name, iter, render, gamma=.98, seed=0, learning_rate=1e-3, target_reward=100, play=False):
     env = gym.make(env_name)
 
     env.seed(seed)
     np.random.seed(seed)
     random.seed(seed)
 
-    model = QNetwork(env_name, env.action_space.n, env.observation_space.shape[0], gamma, seed, log_dir='/tmp/qlearn/lr')
+    model = QNetwork(env_name, env.action_space.n, env.observation_space.shape[0], gamma, seed,
+                     log_dir='/tmp/qlearn/lr')
+    if play:
+        model.load()
+        render = True
 
     m_states = []
     m_actions = []
@@ -124,13 +134,17 @@ def q_learning(env_name, iter, render, gamma=.98, seed=0):
     m_done = []
 
     total_reward = 0
+    success_num = 0
     for i in range(iter):
         observation = env.reset()
         epoch_reward = 0
         while True:
             if render:
                 env.render()
-            action = model.predict_boltzmann([observation])
+            if play:
+                action = model.predict_action([observation])
+            else:
+                action = model.predict_boltzmann([observation])
 
             new_observation, reward, done, _ = env.step(action)
             m_states.append(observation)
@@ -144,16 +158,28 @@ def q_learning(env_name, iter, render, gamma=.98, seed=0):
 
             batch_size = min(len(m_states), 128)
             indexs = random.sample(list(range(len(m_states))), batch_size)
-            model.train_batch(np.array(m_states)[indexs],
-                              np.array(m_actions)[indexs],
-                              np.array(m_rewards)[indexs],
-                              np.array(m_next_states)[indexs],
-                              np.array(m_done)[indexs])
+
+            if not play:
+                model.train_batch(np.array(m_states)[indexs],
+                                  np.array(m_actions)[indexs],
+                                  np.array(m_rewards)[indexs],
+                                  np.array(m_next_states)[indexs],
+                                  np.array(m_done)[indexs],
+                                  learning_rate)
             observation = new_observation
 
             if done:
                 break
         print("epoch %d reward %d" % (i, epoch_reward))
+
+        if not play and epoch_reward >= target_reward:
+            success_num += 1
+            if success_num == 10:
+                print("Reach the target reward")
+                model.save(i)
+                break
+        else:
+            success_num = 0
 
     print("Average Reward: %d" % (total_reward / iter))
     return total_reward / iter
@@ -164,6 +190,16 @@ if __name__ == '__main__':
     gamma = 1.
     render = False
     env_name = 'CartPole-v0'
+    learning_rate = 1e-3
+    target_reward = 180
+    play = True
 
     seed = 10
-    q_learning(env_name=env_name, iter=epoch, render=render, gamma=gamma, seed=seed)
+    q_learning(env_name=env_name,
+               iter=epoch,
+               render=render,
+               gamma=gamma,
+               seed=seed,
+               learning_rate=learning_rate,
+               target_reward=target_reward,
+               play=play)
